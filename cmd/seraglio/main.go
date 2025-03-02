2853d2b3a269ab43b5f30987ceac79a6155afe92
Juliet <m@juli.ee>
2025-03-01 23:48:44 -0800

    easier logic, fuck it

--- cmd/seraglio/main.go
+++ cmd/seraglio/main.go
@@ -178,13 +178,13 @@ func (b *Bot) GuildCreate(s *discordgo.Session, gc *discordgo.GuildCreate) {
 
 func (b *Bot) userJoin(s *discordgo.Session, vs *discordgo.VoiceStateUpdate) {
 	if vs.ChannelID != "" {
-		var exists bool
-		if err := b.db.Exec("SELECT EXISTS(SELECT 1 FROM user_sessions WHERE user_id = ? AND end_time IS NULL)", vs.UserID).Scan(&exists).Error; err != nil {
+		var rows []UserSession
+		if err := b.db.Exec("SELECT * FROM user_sessions WHERE user_id = ? AND end_time IS NULL)", vs.UserID).Scan(&rows).Error; err != nil {
 			log.Printf("Error checking for existing user session: %v", err)
 			return
 		}
 
-		if exists {
+		if len(rows) > 0 {
 			return
 		}
 
